fe2a1e16736edd9dd38f013106ebc24a5ca4ff94
Kyle Robertson <me@iwolfking.xyz>
2024-02-05 15:35:58 -0600

    Fix some v0.9.9 issues

--- kubejs/server_scripts/woldsvaults/ingredients.js
+++ kubejs/server_scripts/woldsvaults/ingredients.js
@@ -1,3 +1,14 @@
 onEvent('recipes', event => {
     event.shapeless('the_vault:chromatic_gold_ingot', ['4x minecraft:gold_ingot', 'the_vault:vault_essence_1'])
+    event.shaped(Item.of('the_vault:crystal_seal_unhinged'),
+        [
+            'XGX',
+            'XRX',
+            'XBX'
+        ], {
+        G: 'the_vault:packed_vault_meat_block',
+        X: 'the_vault:dreamstone',
+        R: 'the_vault:crystal_seal_hunter',
+        B: 'the_vault:extraordinary_painite'
+    })
 })
