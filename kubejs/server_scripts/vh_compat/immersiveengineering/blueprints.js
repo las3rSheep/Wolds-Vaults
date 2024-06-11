7b01fb0914f1f740f5c2d22aa4df21f431b00d5d
Kyle Robertson <me@iwolfking.xyz>
2024-06-11 10:55:21 -0500

    Implement most of the Immersive Engineering recipes

---
+++ kubejs/server_scripts/vh_compat/immersiveengineering/blueprints.js
@@ -0,0 +1,100 @@
+
+onEvent("recipes", event => {
+    event.custom({
+        "type": "immersiveengineering:blueprint",
+        "inputs": [
+            {
+                "tag": "forge:treated_wood_slab"
+            },
+            {
+                "item": "the_vault:perfect_larimar"
+            },
+            {
+                "tag": "forge:dusts/redstone"
+            },
+            {
+                "tag": "forge:wires/electrum"
+            },
+            {
+                "item": "the_vault:vault_diamond"
+            }
+        ],
+        "category": "components",
+        "result": {
+            "item": "immersiveengineering:component_electronic"
+        }
+    }).id('immersiveengineering:blueprint/component_electronic')
+
+    event.custom({
+        "type": "immersiveengineering:blueprint",
+        "inputs": [
+            {
+                "tag": "forge:glass"
+            },
+            {
+                "tag": "forge:plates/nickel"
+            },
+            {
+                "tag": "forge:wires/copper"
+            },
+            {
+                "item": "the_vault:vault_essence_1"
+            },
+            {
+                "item": "the_vault:vault_diamond"
+            }
+        ],
+        "category": "components",
+        "result": {
+            "item": "immersiveengineering:electron_tube",
+            "count": 3
+        }
+    }).id('immersiveengineering:blueprint/electron_tube')
+
+    event.custom({
+        "type": "immersiveengineering:blueprint",
+        "inputs": [
+            {
+                "tag": "forge:plates/plastic"
+            },
+            {
+                "count": 2,
+                "base_ingredient": {
+                    "item": "immersiveengineering:electron_tube"
+                }
+            },
+            {
+                "tag": "forge:wires/aluminum"
+            },
+            {
+                "item": "the_vault:chromatic_gold_ingot"
+            },
+            {
+                "item": "the_vault:vault_diamond"
+            }
+        ],
+        "category": "components",
+        "result": {
+            "item": "immersiveengineering:component_electronic_adv"
+        }
+    }).id('immersiveengineering:blueprint/component_electronic_adv')
+
+    event.custom({
+        "type": "immersiveengineering:blueprint",
+        "inputs": [
+            {
+                "tag": "forge:plates/plastic"
+            },
+            {
+                "tag": "forge:plates/copper"
+            },
+            {
+                "item": "the_vault:magic_silk"
+            }
+        ],
+        "category": "components",
+        "result": {
+            "item": "immersiveengineering:circuit_board"
+        }
+    }).id('immersiveengineering:blueprint/circuit_board')
+})
