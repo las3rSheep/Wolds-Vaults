723eb806219d10847cfc9660ef2a791a8a01591b
Kyle Robertson <me@iwolfking.xyz>
2024-09-17 13:10:03 -0500

    Fix CC recipes

--- kubejs/server_scripts/vh_compat/computercraft/recipes.js
+++ kubejs/server_scripts/vh_compat/computercraft/recipes.js
@@ -5,7 +5,7 @@ onEvent("recipes", event => {
         event.remove({ 'output': `${id}` })
     })
 
-    event.replaceInput({ id: 'computercraft:computer_normal' }, 'the_vault:echo_pog', 'the_vault:pog_prism')
+    //event.replaceInput({ id: 'computercraft:computer_normal' }, 'the_vault:echo_pog', 'the_vault:pog_prism')
 
 
 
